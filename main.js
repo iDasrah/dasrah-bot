57be248d56b93ad5da1c2e3939706cffa142d365
Mathéo Picouleau <dasrah.dev@gmail.com>
2021-05-07 14:05:09 +0200

    amélioration du code

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 // imports
 const Discord = require('discord.js');
 const fs = require('fs');
-const config = require('./config.json');
-
-// config consts
-const prefix = config.prefix;
-const token = config.token;
-const messages = config.messages;
-const botInfo = config.bot_info;
+const { prefix, token, messages, bot_info } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.on('ready', () => {
-	console.log(`${config.bot_info.name}: I'm ready !`);
+	console.log(`${bot_info.name}: I'm ready !`);
 
 	// status
 	client.user.setPresence({
 		activity: {
-			name: `${config.prefix}help pour de l'aide`
+			name: `${prefix}help pour de l'aide`
 		}
 	})
 
