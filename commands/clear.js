53d30e0640531d71b402a3714594ff1911e1f9b0
Mathéo Picouleau <dasrah.dev@gmail.com>
2021-05-11 12:44:04 +0200

    argument optionnel

--- commands/clear.js
+++ commands/clear.js
@@ -4,16 +4,23 @@ module.exports = {
   name: "clear",
   description: "supprime un nombre donné de messages.",
   async execute(message, args) {
-    if(!args.length) return message.reply(config.messages['no-argument-error']);
-    if(isNaN(args[0])) return message.reply(config.messages['NaN-arg-error']);
+    if(args[0]) {
+      if(isNaN(args[0])) return message.reply(config.messages['NaN-arg-error']);
 
-    if(args[0] > 100) return message.reply(config.messages['too-much-clear-error']);
-    if(args[0] < 1) return message.reply(config.messages['not-enough-clear-error']);
+      if(args[0] > 100) return message.reply(config.messages['too-much-clear-error']);
+      if(args[0] < 1) return message.reply(config.messages['not-enough-clear-error']);
+    }
 
     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(config.messages['no-permission-error']);
 
-    await message.channel.messages.fetch({limit: args[0]}).then(messages => {
-      message.channel.bulkDelete(messages);
-    })
+    if(!args.length) {
+      await message.channel.messages.fetch({limit: 100}).then(messages => {
+        message.channel.bulkDelete(messages);
+      });
+    } else {
+      await message.channel.messages.fetch({limit: args[0]}).then(messages => {
+        message.channel.bulkDelete(messages);
+      });
+    }
   }
 }
