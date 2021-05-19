8d536b97551977a0aee89cc1b2e1b2d79baf58b7
Mathéo Picouleau <dasrah.dev@gmail.com>
2021-05-19 22:34:32 +0200

    message de bienvenue/départ, amélioration du code

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+"use strict";
+
 // imports
-const { Client, Collection } = require('discord.js');
-const { prefix, token, messages, bot_info, roles } = require('./json/config.json');
+const { Client, Collection, MessageEmbed } = require('discord.js');
+const { prefix, token, bot_messages, bot_info, roles } = require('./json/config.json');
 const { readdirSync } = require('fs');
 
 const client = new Client();
@@ -19,18 +21,29 @@ const loadCommands = (dir = "./commands/") => {
 
 loadCommands();
 
+client.on('ready', () => {
+	console.log(`${bot_info.name}: I'm ready !`);
+
+	// status
+	client.user.setPresence({
+		activity: {
+			name: `${prefix}help pour de l'aide`
+		}
+	})
+});
+
 // on member join
 client.on('guildMemberAdd', (member) => {
-	const channel = member.guild.channels.cache.find(ch => ch.name === 'portail');
-	const role = message.guild.roles.cache.find(role => role.name === roles['on-join']);
+	const channel = member.guild.channels.cache.get('840203319254188063');
+	const role = member.guild.roles.cache.find((role) => role.name === roles['on-join']);
 	if(!channel) return;
-	channel.send(`Salut mec ! ${member}`);
+	channel.send(`${member}, salut mec !`);
 	member.roles.add(role);
 });
 
 // on member quit
 client.on('guildMemberRemove', (member) => {
-	const channel = member.guild.channels.cache.find(ch => ch.name === 'portail');
+	const channel = member.guild.channels.cache.get('840203319254188063');
 	if(!channel) return;
 	channel.send(`C'est ça, casse toi, ${member}.`)
 });
@@ -43,26 +56,26 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
-	if(!client.commands.has(commandName)) return message.reply(messages['invalid-command-error']);
+	if(!client.commands.has(commandName)) return message.reply(bot_messages['invalid-command-error']);
 
 	const command = client.commands.get(commandName);
+
+	if(command.help.args && !args.length) {
+		const embed = new MessageEmbed()
+		.setTitle("ERROR: NO-ARGUMENT-ERROR")
+		.setColor('#A80506')
+		.setDescription(bot_messages['no-argument-error'])
+		.addField('Usage', `${prefix}${command.help.name} ${command.help.usage}`);
+		return message.reply(embed);
+	}
+	else if(!message.member.hasPermission(command.help.permission)) return message.reply(bot_messages['no-permission-error']);
 
 	try {
 		command.run(client, message, args);
 	} catch(error) {
 		console.error(error);
-		message.reply(messages['command-execution-error']);
+		message.reply(bot_messages['command-execution-error']);
 	}
 });
 
-client.on('ready', () => {
-	console.log(`${bot_info.name}: I'm ready !`);
-
-	// status
-	client.user.setPresence({
-		activity: {
-			name: `${prefix}help pour de l'aide`
-		}
-	})
-});
 client.login(token);
