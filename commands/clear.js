505e5bb9578db053703d613b38d92abdb9cebaa4
Mathéo Picouleau <dasrah.dev@gmail.com>
2021-05-19 18:51:24 +0200

    améliorations

--- commands/clear.js
+++ commands/clear.js
@@ -1,17 +1,17 @@
-const config = require('../config.json');
+const { messages } = require('../config.json');
 
 module.exports = {
   name: "clear",
-  description: "supprime un nombre donné de messages.",
-  async execute(message, args) {
+  description: "Clear le channel",
+  async execute(client, message, args) {
     if(args[0]) {
-      if(isNaN(args[0])) return message.reply(config.messages['NaN-arg-error']);
+      if(isNaN(args[0])) return message.reply(messages['NaN-arg-error']);
 
-      if(args[0] > 100) return message.reply(config.messages['too-much-clear-error']);
-      if(args[0] < 1) return message.reply(config.messages['not-enough-clear-error']);
+      if(args[0] > 100) return message.reply(messages['too-much-clear-error']);
+      if(args[0] < 1) return message.reply(messages['not-enough-clear-error']);
     }
 
-    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(config.messages['no-permission-error']);
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(messages['no-permission-error']);
 
     if(!args.length) {
       await message.channel.messages.fetch({limit: 100}).then(messages => {
