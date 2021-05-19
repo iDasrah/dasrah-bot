c583f9eb8d56f2e352ac5d761ba4dd453fbce501
Mathéo Picouleau <dasrah.dev@gmail.com>
2021-05-19 20:02:50 +0200

    déplacements des fichiers

--- commands/help.js
+++
@@ -1,9 +0,0 @@
-const { messages } = require('../config.json');
-
-module.exports = {
-  name: 'help',
-  description: 'Commande d\'aide',
-  execute(client, message, args) {
-    message.reply(messages['disabled-command']);
-  }
-}
