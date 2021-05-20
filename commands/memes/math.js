aea701cf3f968739473578a2274e86d2a1034347
Mathéo Picouleau <dasrah.dev@gmail.com>
2021-05-20 11:41:44 +0200

    ajout memes maths

---
+++ commands/memes/math.js
@@ -0,0 +1,22 @@
+const { memes } = require('../../json/maths.json');
+const { MessageEmbed } = require('discord.js');
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+module.exports.run = (client, message, args) => {
+  const embed = new MessageEmbed()
+    .setColor('#41B64C')
+    .setTitle('MATH MEME GENERATOR')
+    .setTimestamp()
+    .setImage(memes[random(0, memes.length)].url);
+    message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: 'math',
+  description: 'Génère un meme de maths aléatoire'
+}
