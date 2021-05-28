803d12e64c48cdbd40c2d843150dd4cd86d8ad3c
Dasrah <dasrah.dev@gmail.com>
2021-05-28 15:52:21 +0200

    ajout commande ban

---
+++ commands/moderation/ban.js
@@ -0,0 +1,25 @@
+module.exports.run = (client, message, args) => {
+  const reason = args.splice(1).join(' ') || 'Raison non spécifiée.';
+  const taggedUser = message.mentions.users.first();
+  embed = new MessageEmbed()
+  .setTitle('MEMBRE BANNI')
+  .setDescription('Un camarade a été banni...')
+  .addField('Membre', taggedUser, true)
+  .addField('Raison', reason)
+  .setTimestamp();
+
+  if(!taggedUser) return message.reply(bot_messages['no-user-tagged']);
+  taggedUser ? message.guild.member(taggedUser).ban(reason) : message.reply(bot_messages['user-not-found']);
+
+  return client.channels.cache.get('846288755480592435').send(embed);
+}
+
+module.exports.help = {
+  name: 'ban',
+  description: 'Bannir un membre',
+  args: [true, [100, '']],
+  usage: '<membre> [raison]',
+  category: '',
+  aliases: ['b'],
+  permission: 'BAN_MEMBERS'
+}
