e2b3fd29f23ae037cf36f67b0ba615db4002e130
Dasrah <dasrah.dev@gmail.com>
2021-06-08 20:04:47 +0200

    petit ajout

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -1,15 +1,17 @@
 const { roles } = require('../../json/config.json');
 
 module.exports = (client, member) => {
-	const channel = member.guild.channels.cache.get('840203319254188063');
+	const welcomeChannel = member.guild.channels.cache.get('840203319254188063');
 	const role = member.guild.roles.cache.find((role) => role.name === roles['on-join']);
 	const guild = client.guilds.cache.get('825753614898167848');
 	const roadToChannel = guild.channels.cache.get('851794676637630494');
+	const discussChannel = guild.channels.cache.get('826673568930594816');
 	const currentRoadTo = 50;
 
-	if (!channel) return;
-	channel.send(`${member}, salut mec !`);
+	welcomeChannel.send(`${member}, salut mec !`);
 	member.roles.add(role);
+
+	discussChannel.send(`Souhaitez tous la bienvenue à ${member}, s'il vous plait !`)
 
 	// road to bar
 	clearChannel(roadToChannel);
