b3eb5c6bce2590842966de716e7bf3c11790f01d
Dasrah <dasrah.dev@gmail.com>
2021-06-09 14:01:49 +0200

    compactisation du code auto-role

--- events/reactions/messageReactionAdd.js
+++ events/reactions/messageReactionAdd.js
@@ -7,137 +7,19 @@ module.exports = (client, messageReaction, user) => {
 	const emoji = messageReaction.emoji.name;
 	const channel = message.guild.channels.cache.find((c) => c.id === '846459783674593371');
 
-	const roleList = roles(client);
+	const roleList = [].concat.apply([], roles(client));
 
 	if (member.user.bot) return;
 	if (emoteList.includes(emoji) && message.channel.id === channel.id) {
-		switch (emoji) {
-			case 'bronze1':
-				member.roles.add(bronze);
+		for (let emote of emoteList) {
+			if (emote === emoji) {
+				const role = roleList[emoteList.indexOf(emote)];
+				member.roles.add(role);
 				message.channel
-					.send(`${member}, vous avez reçu le rôle ${bronze.toString()}.`)
+					.send(`${member}, vous avez reçu le rôle ${role.toString()}.`)
 					.then((msg) => msg.delete({ timeout: 3000 }));
 				break;
-			case 'silv1':
-				member.roles.add(silver);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${silver.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'gold1':
-				member.roles.add(gold);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${gold.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'plat1':
-				member.roles.add(platinum);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${platinum.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'diam1':
-				member.roles.add(diamond);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${diamond.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'c1':
-				member.roles.add(champion);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${champion.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'gc1':
-				member.roles.add(grandChampion);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${grandChampion.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'ssl':
-				member.roles.add(ssl);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${ssl.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'sign_male':
-				member.roles.add(man);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${man.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'sign_female':
-				member.roles.add(woman);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${woman.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'playstation':
-				member.roles.add(playstation);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${playstation.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'windows_10':
-				member.roles.add(windows);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${windows.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'xbox':
-				member.roles.add(xbox);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${xbox.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'switch':
-				member.roles.add(nswitch);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${nswitch.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'developer':
-				member.roles.add(developer);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${developer.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'flstudio':
-				member.roles.add(beatmaker);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${beatmaker.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'zerotwo':
-				member.roles.add(anime);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${anime.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'controller':
-				member.roles.add(videogames);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${videogames.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'sword':
-				member.roles.add(minecraft);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${minecraft.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'rocketleague':
-				member.roles.add(rocketleague);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${rocketleague.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'overwatch':
-				member.roles.add(overwatch);
-				message.channel
-					.send(`${member}, vous avez reçu le rôle ${overwatch.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
+			}
 		}
 	}
 
--- events/reactions/messageReactionRemove.js
+++ events/reactions/messageReactionRemove.js
@@ -6,141 +6,20 @@ module.exports = (client, messageReaction, user) => {
 	const member = message.guild.members.cache.get(user.id);
 	const emoji = messageReaction.emoji.name;
 	const channel = message.guild.channels.cache.find((c) => c.id === '846459783674593371');
-	const rolesCache = message.guild.roles.cache;
 
-	const roleList = roles(client);
+	const roleList = [].concat.apply([], roles(client));
 
 	if (member.user.bot) return;
-	if (emoteList.includes(emoji) &&
-		message.channel.id === channel.id
-	) {
-		switch (emoji) {
-			case 'bronze1':
-				member.roles.remove(bronze);
+	if (emoteList.includes(emoji) && message.channel.id === channel.id) {
+		for (let emote of emoteList) {
+			if (emote === emoji) {
+				const role = roleList[emoteList.indexOf(emote)];
+				member.roles.remove(role);
 				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${bronze.toString()}.`)
+					.send(`${member}, vous n'avez plus le rôle ${role.toString()}.`)
 					.then((msg) => msg.delete({ timeout: 3000 }));
 				break;
-			case 'silv1':
-				member.roles.remove(silver);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${silver.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'gold1':
-				member.roles.remove(gold);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${gold.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'plat1':
-				member.roles.remove(platinum);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${platinum.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'diam1':
-				member.roles.remove(diamond);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${diamond.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'c1':
-				member.roles.remove(champion);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${champion.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'gc1':
-				member.roles.remove(grandChampion);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${grandChampion.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'ssl':
-				member.roles.remove(ssl);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${ssl.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'sign_male':
-				member.roles.remove(man);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${man.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'sign_female':
-				member.roles.remove(woman);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${woman.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'playstation':
-				member.roles.remove(playstation);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${playstation.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'windows_10':
-				member.roles.remove(windows);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${windows.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'xbox':
-				member.roles.remove(xbox);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${xbox.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'switch':
-				member.roles.remove(nswitch);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${nswitch.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'developer':
-				member.roles.remove(developer);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${developer.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'flstudio':
-				member.roles.remove(beatmaker);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${beatmaker.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'zerotwo':
-				member.roles.remove(anime);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${anime.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'controller':
-				member.roles.remove(videogames);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${videogames.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'sword':
-				member.roles.remove(minecraft);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${minecraft.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'rocketleague':
-				member.roles.remove(rocketleague);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${rocketleague.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
-			case 'overwatch':
-				member.roles.remove(overwatch);
-				message.channel
-					.send(`${member}, vous n'avez plus le rôle ${overwatch.toString()}.`)
-					.then((msg) => msg.delete({ timeout: 3000 }));
-				break;
+			}
 		}
 	}
 };
