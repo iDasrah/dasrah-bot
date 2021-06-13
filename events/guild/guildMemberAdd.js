940001c3b443f05bae393b9a8f8facd429b7a3f5
Dasrah <dasrah.dev@gmail.com>
2021-06-13 22:37:40 +0200

    déplacement fichiers

---
+++ events/guild/guildMemberAdd.js
@@ -0,0 +1,28 @@
+const { ROLES, CHANNELS, GUILD } = require('../../utils/consts');
+const { clearChannel, loadBar } = require('../../utils/functions');
+
+module.exports = async (client, member) => {
+	const newMember = {
+		guildID: member.guild.id,
+		guildName: member.guild.name,
+		userID: member.id,
+		userName: member.user.username,
+	};
+
+	await client.createUser(newMember);
+
+	const welcomeChannel = member.guild.channels.cache.get(CHANNELS.WELCOMECHANNEL);
+	const role = member.guild.roles.cache.get(ROLES.ONJOIN);
+	const guild = client.guilds.cache.get(GUILD.ID);
+	const roadToChannel = guild.channels.cache.get(CHANNELS.ROADTOCHANNEL);
+	const discussChannel = guild.channels.cache.get(CHANNELS.DISCUSSCHANNEL);
+
+	welcomeChannel.send(`${member}, salut mec !`);
+	member.roles.add(role);
+
+	discussChannel.send(`Souhaitez tous la bienvenue à ${member}, s'il vous plait !`);
+
+	// road to bar
+	clearChannel(roadToChannel);
+	loadBar(roadToChannel, guild);
+};
